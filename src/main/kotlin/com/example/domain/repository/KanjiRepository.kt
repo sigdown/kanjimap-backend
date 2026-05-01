7f75773ec028d6dd907f7e191e487ba90706ea10
argform <vasilybichutsky@gmail.com>
2026-05-01 17:26:15 +0300

    feature: dict repo interface

--- src/main/kotlin/com/example/domain/repository/KanjiRepository.kt
+++ src/main/kotlin/com/example/domain/repository/KanjiRepository.kt
@@ -1,5 +1,12 @@
 package com.example.domain.repository
 
-interface KanjiRepository {
+import com.example.domain.model.Kanji
+import com.example.domain.model.KanjiMeaning
+import com.example.domain.model.Word
 
+interface KanjiRepository {
+    suspend fun getById(kanjiId: Long) : Kanji?
+    suspend fun search(query: String): List<Kanji>
+    suspend fun getMeanings(kanjiId: Long): List<KanjiMeaning>
+    suspend fun getWordsByKanjiId(kanjiId: Long): List<Word>
 }
--- src/main/kotlin/com/example/domain/repository/WordRepository.kt
+++ src/main/kotlin/com/example/domain/repository/WordRepository.kt
@@ -1,5 +1,14 @@
 package com.example.domain.repository
 
-interface WordRepository {
+import com.example.domain.model.Kanji
+import com.example.domain.model.Word
+import com.example.domain.model.WordMeaning
+import com.example.domain.model.WordRelation
 
+interface WordRepository {
+    suspend fun getById(wordId: Long): Word?
+    suspend fun search(query: String): List<Word>
+    suspend fun getMeanings(wordId: Long): List<WordMeaning>
+    suspend fun getRelations(wordId: Long): List<WordRelation>
+    suspend fun getKanjis(wordId: Long): List<Kanji>
 }
